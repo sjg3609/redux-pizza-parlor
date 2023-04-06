39e30b83a5c9235236014f92352eb85ad6bbcef3
Sam Gossie <sjg3609@gmail.com>
2023-04-06 10:56:53 -0500

    Added axios POST for customer information

--- src/components/CustomerForm/CustomerForm.jsx
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -1,13 +1,44 @@
+import axios from 'axios';
+
 function CustomerForm() {
+
+    const submitInfo = (event) => {
+        event.preventDefault();
+        addCustomer();
+        clearAllFields();
+    }
+
+    const addCustomer = () => {
+        axios({
+            method: 'POST',
+            url: '/orders',
+            data: {}
+        }).then((response) => {
+            console.log(response);
+        }).catch((error) => {
+            console.log(`Error in POST for CustomerForm ${error}`)
+            alert('Something went wrong!');
+        });
+    }
+
+    const clearAllFields = () => {
+        
+    } 
 
     return (
         <div>
             <h2>Step 2: Customer Information</h2>
-            <form>
+            <form onSubmit="submitInfo">
                 <input type="text" value="Name"/>
+                <br/>
                 <input type="text" value="Street Address"/>
+                <br/>
                 <input type="text" value="City" />
+                <br/>
                 <input type="text" value="Zip Code"/>
+                <br/>
+                <input type="checkbox" value="Pickup"/>Pickup
+                <input type="checkbox" value="Delivery"/>Delivery
             </form>
             <button onClick="">Next</button>
         </div>
