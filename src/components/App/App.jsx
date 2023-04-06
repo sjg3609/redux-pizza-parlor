58c2371c80f1f37bc56cf94d473e2a8fea76b450
Sam Gossie <sjg3609@gmail.com>
2023-04-06 10:20:25 -0500

    Added Routes for navigation

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -9,7 +10,28 @@ function App() {
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-  
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/checkout">Checkout</Link>
+          </li>
+        </ul>
+        <Route exact path="/">
+
+        </Route>
+        <Route exact path="/customersInfo">
+
+        </Route>
+        <Route exact path="/checkout">
+
+        </Route>
+        <Route exact path="admin">
+          
+        </Route>
+      </Router>
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
   
